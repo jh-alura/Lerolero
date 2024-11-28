03b24b55beb79743bdd728a5bae3940bfd541c5b
jh-alura <00001099499227sp@al.educacao.sp.gov.br>
2024-11-28 15:28:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quem inventou o Teorema de Tales?',
+    'Tales de Mileto'
+)
+
+criaCartao(
+    'Entreterimento',
+    'Qual é o filme mais assistido no mundo?',
+    'Avatar'
+)
+
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'É uma linguagem de programação utilizada para programar páginas web'
+)
+
+criaCartao(
+    'Esportes',
+    'Quando a NBA foi criada?',
+    '1946'
+)
